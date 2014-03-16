47c599b807c6e26c90b09b111a64190f0d97ae10
uupaa <uupaa.js@gmail.com>
2014-03-17 07:03:57 +0900

    update plato files

--- lint/plato/report.js
+++ lint/plato/report.js
@@ -1,1 +1,63 @@
-__report = {"summary":{"total":{"sloc":445,"maintainability":64.96269436273663},"average":{"sloc":445,"maintainability":"64.96"}},"reports":[{"info":{"file":"./lib/Task.js","fileShort":"./lib/Task.js","fileSafe":"__lib_Task_js","link":"files/__lib_Task_js/index.html"},"jshint":{"messages":0},"complexity":{"aggregate":{"line":4,"complexity":{"sloc":{"physical":445,"logical":232},"cyclomatic":52,"halstead":{"operators":{"distinct":35,"total":587,"identifiers":["__stripped__"]},"operands":{"distinct":197,"total":743,"identifiers":["__stripped__"]},"length":1330,"vocabulary":232,"difficulty":66.00253807106598,"volume":10451.114723519671,"effort":689800.0974241854,"bugs":3.4837049078398903,"time":38322.227634676965},"params":37}},"module":"./lib/Task.js","maintainability":64.96269436273663}}]}
+__report = {
+  "reports": [
+    {
+      "info": {
+        "file": "./lib/Task.js",
+        "fileShort": "./lib/Task.js",
+        "fileSafe": "__lib_Task_js",
+        "link": "files/__lib_Task_js/index.html"
+      },
+      "jshint": {
+        "messages": 0
+      },
+      "complexity": {
+        "aggregate": {
+          "line": 4,
+          "complexity": {
+            "sloc": {
+              "physical": 445,
+              "logical": 232
+            },
+            "cyclomatic": 52,
+            "halstead": {
+              "operators": {
+                "distinct": 35,
+                "total": 587,
+                "identifiers": [
+                  "__stripped__"
+                ]
+              },
+              "operands": {
+                "distinct": 197,
+                "total": 743,
+                "identifiers": [
+                  "__stripped__"
+                ]
+              },
+              "length": 1330,
+              "vocabulary": 232,
+              "difficulty": 66.00253807106598,
+              "volume": 10451.114723519671,
+              "effort": 689800.0974241854,
+              "bugs": 3.4837049078398903,
+              "time": 38322.227634676965
+            },
+            "params": 37
+          }
+        },
+        "module": "./lib/Task.js",
+        "maintainability": 64.96269436273663
+      }
+    }
+  ],
+  "summary": {
+    "total": {
+      "sloc": 445,
+      "maintainability": 64.96269436273663
+    },
+    "average": {
+      "sloc": 445,
+      "maintainability": "64.96"
+    }
+  }
+}
