e6d72c30aa9d1733e4b17d3dfa32d47489d1b0b4
uupaa <uupaa.js@gmail.com>
2014-12-12 09:14:35 +0900

    new cli

--- test/index.node.js
+++ test/index.node.js
@@ -1,9 +1,8 @@
-require("../node_modules/uupaa.nodemodule.js/lib/NodeModule.js");
-require("../node_modules/uupaa.reflection.js/lib/Reflection.js");
-require("../node_modules/uupaa.console.js/lib/Console.js");
-require("../node_modules/uupaa.help.js/lib/Help.js");
-require("../node_modules/uupaa.valid.js/lib/Valid.js");
+require("../../WebModule/lib/Console.js");
+require("../../WebModule/lib/Help.js");
+require("../../WebModule/lib/Valid.js");
+require("../../WebModule/lib/TestTask.js");
+require("../../WebModule/lib/Test.js");
 require("../lib/Task.js");
-require("../node_modules/uupaa.test.js/lib/Test.js");
 require("../release/Task.min.js");
-require("./test.js");
+require("./testcase.js");
